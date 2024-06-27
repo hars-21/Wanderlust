cad822aa614cbf4b684e7b6bca6ce638940dc92c
Harshil Gupta <harshilgupta.2005@gmail.com>
2024-06-27 23:43:56 +0530

    filter bar update

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,35 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-  "use strict";
+	"use strict";
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	const forms = document.querySelectorAll(".needs-validation");
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
+	// Loop over them and prevent submission
+	Array.from(forms).forEach((form) => {
+		form.addEventListener(
+			"submit",
+			(event) => {
+				if (!form.checkValidity()) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
 
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
+				form.classList.add("was-validated");
+			},
+			false
+		);
+	});
 })();
+
+// Filter scroll buttons
+
+const slideRight = document.getElementById("slideRight");
+const slideLeft = document.getElementById("slideLeft");
+
+slideRight.onclick = function () {
+	document.getElementById("filter-container").scrollLeft += 200;
+};
+slideLeft.onclick = function () {
+	document.getElementById("filter-container").scrollLeft -= 200;
+};
